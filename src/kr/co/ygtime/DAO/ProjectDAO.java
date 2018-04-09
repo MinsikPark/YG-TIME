cd0e55a668b4fe741cddc90c500f3b2776950cb3
nyJeon <yellowmoon720@naver.com>
2018-04-09 19:07:51 +0900

    ProjectDAO , ListDAO 틀 ,  주석삽입

--- src/kr/co/ygtime/DAO/ProjectDAO.java
+++ src/kr/co/ygtime/DAO/ProjectDAO.java
@@ -18,54 +18,63 @@ public class ProjectDAO {
 
 	DataSource ds;
 
+	// 프로젝트DAO 생성자
 	public ProjectDAO() {
 		super();
 	}
 
-	public int projectInsert(ProjectDTO project) {
+	//프로젝트 생성 Insert
+	public int projectInsert(ProjectDTO project) { 
 
 		return 0;
 	}
-
+	//프로젝트 삭제 Delete
 	public int projectDelete(int projectNum) {
 
 		return 0;
 	}
+	
+	//프로젝트 수정 Update
+	public int projectUpdate(ProjectDTO project) {
 
+		return 0;
+	}
+
+	
+	
+    //프로젝트 조회 select
 	public ProjectDTO projectSelect(int projectNum) {
 
 		return null;
 	}
 
 
-	public int projectUpdate(ProjectDTO project) {
-
-		return 0;
-	}
-
-
+	//모든 프로젝트 select
 	public List<ProjectDTO> allProjectSelect(String userId) {
 
 		return null;
 	}
 	
 	
-	
+	//팀장 조회 select
 	public boolean ownerSelect(int projectNum, String userId) {
 
 		return true;
 	}
 
+	//팀원 삽입 insert
 	public int teamInsert(TeamDTO team) {
 
 		return 0;
 	}
-
+	
+	//팀원 삭제 Delete
 	public int teamDelete(int projectNum, String userId, String outUserId) {
 
 		return 0;
 	}
-
+	
+	//팀원 삭제 Delete
 	public int teamDelete(TeamDTO team) {
 
 		return 0;
