d1710a789b0056f575514bd049556c48731663e9
DESKTOP-UHTAFJN\bitcamp <pms3300@gmail.com>
2018-04-09 19:24:48 +0900

    보드DAO , 회원DAO 함수 틀 잡기

--- src/kr/co/ygtime/DAO/BoardDAO.java
+++ src/kr/co/ygtime/DAO/BoardDAO.java
@@ -7,6 +7,47 @@
 
 package kr.co.ygtime.DAO;
 
-public class BoardDAO {
+import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import kr.co.ygtime.DTO.BoardDTO;
+
+public class BoardDAO {
+	DataSource ds;
+	
+	public BoardDAO() throws NamingException {
+		Context context = new InitialContext();
+		//JNDI 
+		//context : container(was) 안에서 이름기반으로 검색 제공
+		ds = (DataSource)context.lookup("java:comp/env/jdbc/oracle");
+	}
+	
+	//보드 생성
+	public int boardInsert(int boardNum) {
+		return 0;
+	}
+	
+	//보드 검색(선택)
+	public BoardDTO boardSelect(int boardNum) {
+		return null;
+	}
+	
+	//모든 보드 검색
+	public List<BoardDTO> allBoardSelect(int projectNum) {
+		return null;
+	}
+	
+	//보드 수정
+	public int boardUpdate(BoardDTO board) {
+		return 0;
+	}
+	
+	//보드삭제 ( 삭제여부 업데이트 )
+	public int boardDelete(int boardNum) {
+		return 0;
+	}
 }
