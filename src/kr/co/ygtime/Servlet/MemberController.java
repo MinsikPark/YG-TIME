de7aa267a3a2bdbb286e0f2d3a158ea49c4d32d4
nyJeon <yellowmoon720@naver.com>
2018-04-11 16:05:41 +0900

    front 화면

--- src/kr/co/ygtime/Servlet/MemberController.java
+++ src/kr/co/ygtime/Servlet/MemberController.java
@@ -11,8 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import kr.co.ygtime.Action.Action;
 import kr.co.ygtime.Action.ActionForward;
+import kr.co.ygtime.service.member.JoinService;
 
-@WebServlet("/*.member")
+/**
+ * 
+  클래스명 : MemberController
+  날      짜 : 2018. 4. 11.
+  작성자명 : 전 나 영
+ */
+
+@WebServlet("*.member")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -33,31 +41,21 @@ public class MemberController extends HttpServlet {
         String contextPath = request.getContextPath();
         String cmdURI = requestURI.substring(contextPath.length());
 		
-        ActionForward forward = new ActionForward();
+        ActionForward forward = null;
         Action action = null;
         
-        if(cmdURI.equals("/방법1.time")) {
+        if(cmdURI.equals("")) {
         	/* 방법1 : 아무것도 안타고 바로 경로 적기
 			forward.setRedirect(false);
         	forward.setPath("/board/board_write.jsp");
-<<<<<<< HEAD
-        	test3
-=======
-        	jinwon
-        	충돌 테스트를 하겠습니다
-        	확인 작업을 시작합니다
-        	~~
-        	~~~
-        	안녕하세요
-        	박민식이에요
-        	반가워요
-        	진원인 하이하이
-        	방가방가
-        	test4
-        	~~~~
->>>>>>> 9e9748cfeb50066dee9e3e88ae3dbf91bf2f3b0f
+
+        
+        
+        
+        
+
         	*/
-        }else if(cmdURI.equals("/방법2.time")) {
+        }else if(cmdURI.equals("/Join.member")) {
         	/* 방법2 : 서비스 만들어서 태우고 오기
         	action = new 서비스자바파일();
         	try {
@@ -68,7 +66,16 @@ public class MemberController extends HttpServlet {
 				ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎ
 				test3
 			}
-        	*/
+		*/
+         	action = new JoinService();
+        	try {
+				forward = action.execute(request, response);
+			} 
+        	catch (Exception e) {
+				e.printStackTrace();
+			
+			}
+        	
         }else if(cmdURI.equals("/방법2.time")) {
         	/* 방법2 : 서비스 만들어서 태우고 오기
         	action = new 서비스자바파일();
