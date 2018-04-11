e4ed6d5f6d289946fbfa1e9052f4ec251af0cfac
jinwon <jinwon220@naver.com>
2018-04-11 12:13:58 +0900

    서비스 틀

---
+++ src/kr/co/ygtime/service/member/LoginService.java
@@ -0,0 +1,12 @@
+/* 
+    파일명: LoginService.java
+    설명: 프로젝트 내용 추가 Service부
+    작성일: 2018. 4. 11.
+    작성자: 김진원
+*/
+
+package kr.co.ygtime.service.member;
+
+public class LoginService {
+
+}
---
+++ src/kr/co/ygtime/service/project/InviteListService.java
@@ -0,0 +1,12 @@
+/* 
+    파일명: InviteListService.java
+    설명: 프로젝트 내용 추가 Service부
+    작성일: 2018. 4. 11.
+    작성자: 김진원
+*/
+
+package kr.co.ygtime.service.project;
+
+public class InviteListService {
+
+}
