4451dd6655c16cc1e349b3a9694bb4d419d1d498
jinwon <jinwon220@naver.com>
2018-04-11 16:14:27 +0900

    초대수락 -> 팀 insert -> 메세지 delete -> 메인으로!! 초대삭제

--- src/kr/co/ygtime/Servlet/MemberController.java
+++ src/kr/co/ygtime/Servlet/MemberController.java
@@ -11,8 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import kr.co.ygtime.Action.Action;
 import kr.co.ygtime.Action.ActionForward;
+import kr.co.ygtime.service.member.MsgAgreeService;
+import kr.co.ygtime.service.member.MsgDeleteSerivce;
 
-@WebServlet("/*.member")
+@WebServlet("*.member")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -33,94 +35,40 @@ public class MemberController extends HttpServlet {
         String contextPath = request.getContextPath();
         String cmdURI = requestURI.substring(contextPath.length());
 		
-        ActionForward forward = new ActionForward();
+        ActionForward forward = null;
         Action action = null;
         
-        if(cmdURI.equals("/방법1.time")) {
-        	/* 방법1 : 아무것도 안타고 바로 경로 적기
-			forward.setRedirect(false);
+        if(cmdURI.equals("/msgagree.member")) {
+        	action = new MsgAgreeService();
+        	try {
+				forward = action.execute(request, response);
+			} 
+        	catch (Exception e) {
+				e.printStackTrace();
+			}
+        }else if(cmdURI.equals("/msgdel.member")) {
+        	action = new MsgDeleteSerivce();
+        	try {
+				forward = action.execute(request, response);
+			} 
+        	catch (Exception e) {
+				e.printStackTrace();
+			}
+        }else if(cmdURI.equals("/방법2.member")) {
+        	
+        }else if(cmdURI.equals("/방법2.time")) {
+
+        }else if(cmdURI.equals("/방법2.time")) {
+        	
+        }else if(cmdURI.equals("/방법2.time")) {
+
+        }else if(cmdURI.equals("/방법2.time")) {
+        	
+        }else if(cmdURI.equals("/방법2.time")) {
+        	/* 
+        	forward.setRedirect(false);
         	forward.setPath("/board/board_write.jsp");
-<<<<<<< HEAD
-        	test3
-=======
-        	jinwon
-        	충돌 테스트를 하겠습니다
-        	확인 작업을 시작합니다
-        	~~
-        	~~~
-        	안녕하세요
-        	박민식이에요
-        	반가워요
-        	진원인 하이하이
-        	방가방가
-        	test4
-        	~~~~
->>>>>>> 9e9748cfeb50066dee9e3e88ae3dbf91bf2f3b0f
         	*/
-        }else if(cmdURI.equals("/방법2.time")) {
-        	/* 방법2 : 서비스 만들어서 태우고 오기
-        	action = new 서비스자바파일();
-        	try {
-				forward = action.execute(request, response);
-			} 
-        	catch (Exception e) {
-				e.printStackTrace();
-				ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎ
-				test3
-			}
-        	*/
-        }else if(cmdURI.equals("/방법2.time")) {
-        	/* 방법2 : 서비스 만들어서 태우고 오기
-        	action = new 서비스자바파일();
-        	try {
-				forward = action.execute(request, response);
-			} 
-        	catch (Exception e) {
-				e.printStackTrace();
-			}
-        	*/
-        }else if(cmdURI.equals("/방법2.time")) {
-        	/* 방법2 : 서비스 만들어서 태우고 오기
-        	action = new 서비스자바파일();
-        	try {
-				forward = action.execute(request, response);
-			} 
-        	catch (Exception e) {
-				e.printStackTrace();
-			}
-        	*/
-        }else if(cmdURI.equals("/방법2.time")) {
-        	/* 방법2 : 서비스 만들어서 태우고 오기
-        	action = new 서비스자바파일();
-        	try {
-				forward = action.execute(request, response);
-			} 
-        	catch (Exception e) {
-				e.printStackTrace();
-			}
-        	*/
-        }else if(cmdURI.equals("/방법2.time")) {
-        	/* 방법2 : 서비스 만들어서 태우고 오기
-        	action = new 서비스자바파일();
-        	try {
-				forward = action.execute(request, response);
-			} 
-        	catch (Exception e) {
-				e.printStackTrace();
-			}
-        	*/
-        }else if(cmdURI.equals("/방법2.time")) {
-        	/* 방법2 : 서비스 만들어서 태우고 오기
-        	action = new 서비스자바파일();
-        	try {
-				forward = action.execute(request, response);
-			} 
-        	catch (Exception e) {
-				e.printStackTrace();
-			}
-        	*/
-        }else if(cmdURI.equals("/방법2.time")) {
-        	System.out.println("충돌 테스트 ");
         }
         
         
--- src/kr/co/ygtime/Servlet/ProjectController.java
+++ src/kr/co/ygtime/Servlet/ProjectController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/*.project")
+@WebServlet("*.project")
 public class ProjectController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
